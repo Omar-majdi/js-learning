cc3fcfd22dea1c10c07d2a12f5dbe9853b2cacb8
Omar-majdi <omarmajdi115@gmail.com>
2022-04-24 14:57:54 +0300

    Arary sorting.

--- main.js
+++ main.js
@@ -1,26 +1,10 @@
 /*
-  Arrays Methods [Search]
-  - indexOf(Search Element, From Index [Opt])
-  - lastIndexOf(Search Element, From Index [Opt])
-  - includes(valueToFind, fromIndex [Opt]) [ES7]
+  Arrays Methods [Sort]
+  - sort(Function [Opt])
+  - reverse
 */
 
-let myFriends = ['Ahmed', 'Mohamed', 'Sayed', 'Alaa', 'Ahmed'];
+let myFriends = [10, 'Sayed', 'Mohamed', '90', 9000, 100, 20, '10', -20, -10];
 
 console.log(myFriends);
-
-console.log(myFriends.indexOf('Ahmed'));
-console.log(myFriends.indexOf('Ahmed', 2));
-
-console.log(myFriends.lastIndexOf('Ahmed'));
-console.log(myFriends.lastIndexOf('Ahmed', -2));
-
-console.log(myFriends.includes('Ahmed'));
-console.log(myFriends.includes('Ahmed', 2));
-
-if (myFriends.lastIndexOf('Osama') === -1) {
-  console.log('Not Found');
-}
-
-console.log(myFriends.indexOf('Osama'));
-console.log(myFriends.lastIndexOf('Osama'));
+console.log(myFriends.sort().reverse());
