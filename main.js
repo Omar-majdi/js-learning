de3a2b7dca74d6c2f350ca9265eff57f8101c005
Omar-majdi <omarmajdi115@gmail.com>
2022-04-24 17:31:52 +0300

    Loop on sequences, real EX.

--- main.js
+++ main.js
@@ -1,71 +1,14 @@
-// Ezero Website Challenge 1
 /*
-let myFriends = ['Ahmed', 'Elham', 'Osama', 'Gamal'];
-let num = 3;
-
-// Method 1
-myFriends.pop();
-console.log(myFriends); // ["Ahmed", "Elham", "Osama"];
-
-// Method 2
-myFriends.length = num;
-console.log(myFriends); // ["Ahmed", "Elham", "Osama"];
-*/
-// Ezero Website Challenge 2
-/*
-let friends = ['Ahmed', 'Eman', 'Osama', 'Gamal'];
-
-// Write Your Code Here
-friends.pop();
-friends.shift();
-console.log(friends); // ["Eman", "Osama"]
+  Loop
+  - Loop On Sequences
 */
 
-// Ezero Website Challenge 3
-/*
-let arrOne = ['C', 'D', 'X'];
-let arrTwo = ['A', 'B', 'Z'];
-let finalArr = [];
+let myFriends = [1, 2, 'Osama', 'Ahmed', 3, 4, 'Sayed', 6, 'Ali'];
+let onlyNames = [];
 
-// Write One Single Line Of Code
-finalArr = arrOne.concat(arrTwo).sort().reverse();
-console.log(finalArr); // ["Z", "X", "D", "C", "B", "A"]
-*/
-
-// Ezero Website Challenge 4
-// let website = "Go";
-// let words = [`${website}ogle`, "Facebook", ["Elzero", "Web", "School"]];
-
-// console.log(words); // ZERO
-
-// Ezero Website Challenge 5
-/*
-let needle = 'JS';
-let haystack = ['PHP', 'JS', 'Python'];
-console.log(haystack.includes(needle));
-
-// Write 3 Solutions
-if (haystack.includes(needle)) {
-    console.log('found');
+for (let i = 0; i < myFriends.length; i++) {
+  if (typeof myFriends[i] === 'string') {
+    onlyNames.push(myFriends[i]);
+  }
 }
-haystack.shift();
-haystack.pop();
-if (haystack == needle) {
-    console.log('found');
-}
-*/
-/*
-// Ezero Website Challenge 5
-let arr1 = ['A', 'C', 'X'];
-let arr2 = ['D', 'E', 'F', 'Y'];
-let allArrs = [];
-// Your Code Here
-arr1.shift();
-arr1.shift();
-console.log(arr1);
-arr2.shift();
-arr2.shift();
-console.log(arr2);
-allArrs = arr1.concat(arr2).sort();
-console.log(allArrs); // fxy
-*/
+console.log(onlyNames);
