97b262ed371e02eb32f58e470fe80d51a100f4b6
Omar-majdi <omarmajdi115@gmail.com>
2022-04-24 15:49:10 +0300

    Array Challenge 1.

--- main.js
+++ main.js
@@ -1,24 +1,18 @@
 /*
-  Arrays Methods [Joining]
-  - concat(array, array) => Return A New Array
-  - join(Separator)
+  Array Challenge
 */
 
-let myFriends = ['Ahmed', 'Sayed', 'Ali', 'Osama', 'Gamal', 'Ameer'];
-let myNewFriends = ['Samar', 'Sameh'];
-let schoolFriends = ['Haytham', 'Shady'];
+let zero = 0;
 
-let allFriends = myFriends.concat(
-  myNewFriends,
-  schoolFriends,
-  'Gameel',
-  [1, 2]
-);
+let counter = 3;
 
-console.log(allFriends);
+let my = ['Ahmed', 'Mazero', 'Elham', 'Osama', 'Gamal', 'Ameer'];
 
-console.log(allFriends.join());
-console.log(allFriends.join(''));
-console.log(allFriends.join(' @ '));
-console.log(allFriends.join('|'));
-console.log(allFriends.join('|').toUpperCase());
+// Write Code Here
+
+console.log(my.slice(zero, ++counter).reverse()); // ["Osama", "Elham", "Mazero", "Ahmed"];
+console.log(my.slice(++zero, --counter).reverse()); // ["Elham", "Mazero"]
+my.splice(--zero, counter + counter, 'Elzero');
+console.log(my); // "Elzero"
+my.splice(zero, counter + counter, 'rO');
+console.log(my); // "rO"
