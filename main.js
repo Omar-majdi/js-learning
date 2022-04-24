3e0f164afb98ffb507b6c99a6c0f59c65e185c78
Omar-majdi <omarmajdi115@gmail.com>
2022-04-24 12:13:50 +0300

    the length of an array.

--- main.js
+++ main.js
@@ -1,24 +1,12 @@
 /*
-  Arrays
-  - Create Arrays [Two Methods] new Array() + []
-  - Access Arrays Elements
-  - Nested Arrays
-  - Change Arrays Elements
-  - Check For Array Array.isArray(arr);
+  Array Methods
+  - Length
 */
 
-let myFriends = ['Ahmed', 'Mohamed', 'Sayed', ['Marwan', 'Ali']];
+// Index Start From 0 [ 0, 1, 2, 3, 4 ]
 
-console.log(`Hello ${myFriends[0]}`);
-console.log(`Hello ${myFriends[2]}`);
-console.log(`${myFriends[1][2]}`);
-console.log(`Hello ${myFriends[3][1]}`);
-console.log(`${myFriends[3][1][2]}`);
+let myFriends = ['Ahmed', 'Mohamed', 'Sayed', 'Alaa'];
+
+myFriends.length = 2;
 
 console.log(myFriends);
-myFriends[1] = 'Gamal';
-console.log(myFriends);
-myFriends[3] = ['Sameh', 'Ameer'];
-console.log(myFriends);
-
-console.log(Array.isArray(myFriends));
