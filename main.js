f173d539f8e3420333fa9ffb7226fdd776d677ab
Omar-majdi <omarmajdi115@gmail.com>
2022-04-24 16:32:42 +0300

    Elzero Website challenges for arrays.

--- main.js
+++ main.js
@@ -1,18 +1,71 @@
+// Ezero Website Challenge 1
 /*
-  Array Challenge
+let myFriends = ['Ahmed', 'Elham', 'Osama', 'Gamal'];
+let num = 3;
+
+// Method 1
+myFriends.pop();
+console.log(myFriends); // ["Ahmed", "Elham", "Osama"];
+
+// Method 2
+myFriends.length = num;
+console.log(myFriends); // ["Ahmed", "Elham", "Osama"];
+*/
+// Ezero Website Challenge 2
+/*
+let friends = ['Ahmed', 'Eman', 'Osama', 'Gamal'];
+
+// Write Your Code Here
+friends.pop();
+friends.shift();
+console.log(friends); // ["Eman", "Osama"]
 */
 
-let zero = 0;
+// Ezero Website Challenge 3
+/*
+let arrOne = ['C', 'D', 'X'];
+let arrTwo = ['A', 'B', 'Z'];
+let finalArr = [];
 
-let counter = 3;
+// Write One Single Line Of Code
+finalArr = arrOne.concat(arrTwo).sort().reverse();
+console.log(finalArr); // ["Z", "X", "D", "C", "B", "A"]
+*/
 
-let my = ['Ahmed', 'Mazero', 'Elham', 'Osama', 'Gamal', 'Ameer'];
+// Ezero Website Challenge 4
+// let website = "Go";
+// let words = [`${website}ogle`, "Facebook", ["Elzero", "Web", "School"]];
 
-// Write Code Here
+// console.log(words); // ZERO
 
-console.log(my.slice(zero, ++counter).reverse()); // ["Osama", "Elham", "Mazero", "Ahmed"];
-console.log(my.slice(++zero, --counter).reverse()); // ["Elham", "Mazero"]
-my.splice(--zero, counter + counter, 'Elzero');
-console.log(my); // "Elzero"
-my.splice(zero, counter + counter, 'rO');
-console.log(my); // "rO"
+// Ezero Website Challenge 5
+/*
+let needle = 'JS';
+let haystack = ['PHP', 'JS', 'Python'];
+console.log(haystack.includes(needle));
+
+// Write 3 Solutions
+if (haystack.includes(needle)) {
+    console.log('found');
+}
+haystack.shift();
+haystack.pop();
+if (haystack == needle) {
+    console.log('found');
+}
+*/
+/*
+// Ezero Website Challenge 5
+let arr1 = ['A', 'C', 'X'];
+let arr2 = ['D', 'E', 'F', 'Y'];
+let allArrs = [];
+// Your Code Here
+arr1.shift();
+arr1.shift();
+console.log(arr1);
+arr2.shift();
+arr2.shift();
+console.log(arr2);
+allArrs = arr1.concat(arr2).sort();
+console.log(allArrs); // fxy
+*/
