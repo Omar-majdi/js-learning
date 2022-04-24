cc12e053423345af19636f81d4ed79d2e84128e4
Omar-majdi <omarmajdi115@gmail.com>
2022-04-24 17:54:10 +0300

    Nested loops and training.

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 /*
   Loop
-  - Loop On Sequences
+  - Nested Loops
 */
 
-let myFriends = [1, 2, 'Osama', 'Ahmed', 3, 4, 'Sayed', 6, 'Ali'];
-let onlyNames = [];
+let products = ['Keyboard', 'Mouse', 'Pen', 'Pad', 'Monitor'];
 
-for (let i = 0; i < myFriends.length; i++) {
-  if (typeof myFriends[i] === 'string') {
-    onlyNames.push(myFriends[i]);
+let colors = ['Red', 'Green', 'Black'];
+
+let models = [2020, 2021];
+for (i = 0; i < products.length; i++) {
+  console.log('#'.repeat(15));
+  console.log(`# ${products[i]}`);
+  console.log('#'.repeat(15));
+  console.log('Colors: ');
+
+  for (let j = 0; j < colors.length; j++) {
+    console.log(`- ${colors[j]}`);
+  }
+
+  console.log('Models: ');
+  for (let k = 0; k < models.length; k++) {
+    console.log(`- ${models[k]}`);
   }
 }
-console.log(onlyNames);
