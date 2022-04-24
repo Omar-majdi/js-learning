0a47cce0740af6c3635c781bd8cfaea7c3811b54
Omar-majdi <omarmajdi115@gmail.com>
2022-04-24 18:03:32 +0300

    Loop Control.

--- main.js
+++ main.js
@@ -1,25 +1,20 @@
 /*
-  Loop
-  - Nested Loops
+  Loop Control
+  - Break
+  - Continue
+  - Label
 */
 
 let products = ['Keyboard', 'Mouse', 'Pen', 'Pad', 'Monitor'];
 
 let colors = ['Red', 'Green', 'Black'];
 
-let models = [2020, 2021];
-for (i = 0; i < products.length; i++) {
-  console.log('#'.repeat(15));
-  console.log(`# ${products[i]}`);
-  console.log('#'.repeat(15));
-  console.log('Colors: ');
-
-  for (let j = 0; j < colors.length; j++) {
+mainLoop: for (let i = 0; i < products.length; i++) {
+  console.log(products[i]);
+  nestedLoop: for (let j = 0; j < colors.length; j++) {
     console.log(`- ${colors[j]}`);
-  }
-
-  console.log('Models: ');
-  for (let k = 0; k < models.length; k++) {
-    console.log(`- ${models[k]}`);
+    if (colors[j] === 'Green') {
+      break mainLoop;
+    }
   }
 }
