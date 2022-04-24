b9eacdc6fa240aa51d7a0bd97c0412a7c4e5d6f2
Omar-majdi <omarmajdi115@gmail.com>
2022-04-24 15:17:47 +0300

    Arrays slicing methods.

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 /*
-  Arrays Methods [Sort]
-  - sort(Function [Opt])
-  - reverse
+  Arrays Methods [Slicing]
+  - slice(Start [Opt], End [Opt] Not Including End)
+  --- slice() => All Array
+  --- If Start Is Undefined => 0
+  --- Negative Count From End
+  --- If End Is Undefined || > Indexes => Slice To The End Array.length
+  --- Return New Array
+  - splice(Start [Mand], DeleteCount [Opt] [0 No Remove], The Items To Add [Opt])
+  --- If Negative => Start From The End
 */
 
-let myFriends = [10, 'Sayed', 'Mohamed', '90', 9000, 100, 20, '10', -20, -10];
+let myFriends = ['Ahmed', 'Sayed', 'Ali', 'Osama', 'Gamal', 'Ameer'];
+console.log(myFriends);
+console.log(myFriends.slice());
+console.log(myFriends.slice(1));
+console.log(myFriends.slice(1, 3));
+console.log(myFriends.slice(-3));
+console.log(myFriends.slice(1, -2));
+console.log(myFriends.slice(-4, -2));
+console.log(myFriends);
+
+myFriends.splice(1, 2, 'Sameer', 'Samara');
 
 console.log(myFriends);
-console.log(myFriends.sort().reverse());
