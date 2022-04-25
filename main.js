997c9f6f9fa36416dfc4a2d19eec6986c652a813
Omar-majdi <omarmajdi115@gmail.com>
2022-04-25 16:07:19 +0300

    loop challenge.

--- main.js
+++ main.js
@@ -1,19 +1,42 @@
 /*
-  Products Practice
+  Loop Challenge
 */
 
-let products = ['Keyboard', 'Mouse', 'Pen', 'Pad', 'Monitor', 'iPhone'];
-let colors = ['Red', 'Green', 'Blue'];
-let showCount = 3;
+let myAdmins = ['Ahmed', 'Osama', 'Sayed', 'Stop', 'Samera'];
+let myEmployees = [
+  'Amgad',
+  'Samah',
+  'Ameer',
+  'Omar',
+  'Othman',
+  'Amany',
+  'Samia',
+  'Anwar',
+];
 
-document.write(`<h1>Show ${showCount} Products</h1>`);
+document.write(`<div>We Have X Admins</div>`);
 
-for (let i = 0; i < showCount; i++) {
-  document.write(`<div>`);
-  document.write(`<h3>[${i + 1}] ${products[i]}</h3>`);
-  for (let j = 0; j < colors.length; j++) {
-    document.write(`<p>${colors[j]}</p>`);
+let admins_count = 0;
+myAdmins.length = myAdmins.indexOf('Stop');
+document.write(`<div>We Have ${myAdmins.length} Admins</div>`);
+
+document.write(`<hr/>`);
+
+let leader = 0;
+let teamN = 0;
+let workerN = 0;
+for (; leader < myAdmins.length; leader++) {
+  teamN++;
+  document.write(
+    `<div>The admin For Team ${teamN} is ${myAdmins[leader]}</div>`
+  );
+  letter = myAdmins[leader][0];
+  document.write(`<h3>Team Members:</h3>`);
+  for (let workers = 0; workers < myEmployees.length; workers++) {
+    myEmployees[workers].startsWith(letter)
+      ? document.write(`<p>- ${++workerN} ${myEmployees[workers]}</p>`)
+      : (workers = workers);
   }
-  document.write(`<p>${colors.join(' | ')}</p>`);
-  document.write(`</div>`);
+  workerN = 0;
+  document.write(`<hr/>`);
 }
