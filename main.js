e4da2d7c7e2de2ea7c8e3375298dc8ef9d6b096b
Omar-majdi <omarmajdi115@gmail.com>
2022-04-26 15:22:19 +0300

    Function ex 1.

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
 /*
-  Function
-  - What Is Function ?
-  - User-Defined vs Built In
-  - Syntax + Basic Usage
-  - Example From Real Life
-  - Parameter + Argument
-  - Practical Example
+  Function Advanced Examples
 */
 
-function sayHello(userName) {
-  console.log(`Hi ${userName}`);
+function sayHello(userName, age) {
+  if (age < 20) {
+    console.log(`App is Not Suitable For You`);
+  } else {
+    console.log(`Hello ${userName} Your Age is ${age}`);
+  }
 }
 
-sayHello('Osama');
-sayHello('Sayed');
-sayHello('Ali');
+sayHello('Osama', 38);
+sayHello('Sayed', 40);
+sayHello('Ali', 18);
+
+function generateYears(start, end, exclude) {
+  for (let i = start; i <= end; i++) {
+    if (i === exclude) {
+      continue;
+    }
+    console.log(i);
+  }
+}
+
+generateYears(1982, 2021, 2020);
